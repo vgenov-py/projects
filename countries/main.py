4d15a285704016da2f394da2ceee2b3c7bc56353
unknown <genovese.work@gmail.com>
2021-05-06 22:06:59 +0200

    6/5/2021

--- countries/main.py
+++ countries/main.py
@@ -5,14 +5,34 @@ import time
 import concurrent.futures
 
 url = "https://restcountries.eu/rest/v2/all"
+
+historial = "historial.csv"
 # response = req.get(url).json()
+# un comentario
 
 # with open("countries.json", "w", encoding="utf8") as file:
 #     json.dump(response,file, ensure_ascii=False, indent=4)
 
+def all_flags():
+    response = req.get(url).json()
+    flags = map(lambda country: country["flag"], response)
+    with open("all_flags.csv", "w", newline="") as file:
+        csv_writer = csv.writer(file, delimiter= ",")
+        for flag in flags:
+            csv_writer.writerow([flag])
+# all_flags()
+
+
+
 def get_data():
     with open("countries.json", encoding="utf8") as file:
         return json.load(file)
+
+def read_csv(file_to_read):
+    with open(file_to_read, encoding="utf8") as file:
+        csv_reader = csv.reader(file)
+        next(csv_reader)
+        return list(csv_reader)
 
 def by_country(country_name):
     response = req.get(f"https://restcountries.eu/rest/v2/name/{country_name}").json()[0]
@@ -21,6 +41,18 @@ def by_country(country_name):
 def by_region(region):
     response = req.get(f"https://restcountries.eu/rest/v2/region/{region}").json()
     return response
+
+def get_flag(flag, name=False):
+    if name:
+        response = req.get(flag).content
+        with open(f"img/{name}", "wb") as file:
+            file.write(response)
+    else:
+        response = req.get(flag).content
+        name = flag.split("/")[-1]
+        with open(f"all_flags/{name}", "wb") as file:
+            file.write(response)
+    print(f"Se ha guardado la bandera de {name}")
 
 busqueda = by_country("argentina")
 print(busqueda["name"])
@@ -52,11 +84,56 @@ while user != "q":
 
         data = [country["name"], country["capital"], country["region"],
                 country["population"], country["area"], country["languages"][0]["name"], country["flag"]]
-        with open("historial.csv", "a", newline="") as file:
+        with open("historial.csv", "a", newline="", encoding="utf8") as file:
             escritor = csv.writer(file, delimiter=",")
             escritor.writerow(data)
         print(f"{country['name']} con una población de {country['population']} habitantes")
         menu_principal()
         user = choose()
+    
 
+    # OPCIÓN 4: HISTORIAL DE BÚSQUEDA
 
+    #Ejercicio 11:
+    elif user == "4":
+        data = read_csv(historial)
+        for country in data:
+            print(f"name: {country[0]}\npopulation: {country[3]}")
+            print("---------------------------------------------")
+        user = input("Desea descargar las banderas de los países del historial? (Y/N): ").lower()
+        if user == "y":
+            start_without_threading = time.perf_counter()
+            for country in data:
+                    flag = country[-1]
+                    name = flag.split("/")[-1]
+                    get_flag(flag, name)
+                    print(f"se está descargando la bandera de {country[0]}")
+
+            finish_without_threading = time.perf_counter()
+            print(f"Se han descargado {len(data)} banderas SIN Threading {finish_without_threading-start_without_threading} segundos")
+
+            start_with_threading = time.perf_counter()
+            with concurrent.futures.ThreadPoolExecutor() as executor:
+                for country in data:
+                    flag = country[-1]
+                    name = flag.split("/")[-1]
+                    future = executor.submit(get_flag,flag, name)
+                    print(f"se está descargando la bandera de {country[0]}")
+
+            finish_with_threading = time.perf_counter()
+            print(f"Se han descargado {len(data)} banderas CON Threading {finish_with_threading-start_with_threading} segundos")
+            user = choose()
+
+    # todas las banderas del mundo:   
+    elif user == "6":
+        flags = read_csv("all_flags.csv")
+        start = time.perf_counter()
+        with concurrent.futures.ThreadPoolExecutor() as executor:
+            # for flag in flags:
+            #     executor.submit(get_flag, flag[0])
+
+            [executor.submit(get_flag, flag[0]) for flag in flags]
+
+        finish = time.perf_counter()
+        print(f"TIEMPO __> {finish-start}")
+        user = choose()
--- lambdas/lab.py
+++ lambdas/lab.py
@@ -57,17 +57,17 @@ import time
 
 # test = outter("hola")
 # print(test)
-start = time.perf_counter()
-diccionario = {"sumar": lambda a,b: a + b}
-# print(diccionario["sumar"])
+# start = time.perf_counter()
+# diccionario = {"sumar": lambda a,b: a + b}
+# # print(diccionario["sumar"])
 
-def contenedor(msg): # == @property
-    def saludar(msg):
-        return msg
+# def contenedor(msg): # == @property
+#     def saludar(msg):
+#         return msg
 
-    return saludar(msg)
+#     return saludar(msg)
 
-test= contenedor("Otra cosa!")
+# test= contenedor("Otra cosa!")
 # print(test)
 
 
@@ -92,14 +92,85 @@ test= contenedor("Otra cosa!")
 
 # ERROR HANDLING:
 
-a = 1
-b = 2
-if True:
-    print("hola")
+# a = 1
+# b = 2
+# if True:
+#     print("hola")
 
-try: 
-    print(a+b)
-except: 
-    print("No son números")
-finish = time.perf_counter()
-print(finish-start)
+# try: 
+#     print(a+b)
+# except: 
+#     print("No son números")
+# finish = time.perf_counter()
+# print(finish-start)
+
+a = [1,2,3,4]
+#[num for num in a]
+#[num for num in a if num %2 == 0]
+#[num *2 if num %2 == 0 else num for num in a]
+
+
+#OPERADORES TERNARIOS
+a = [1,2,3,4]
+
+#NÚMEROS PARES CON TERNARIO Y COMPREHENSION:
+result_ternario = [num*2 if num %2 == 0 else num for num in a ]
+result = list(map(lambda num: num*2 if num %2 ==0 else num,a))
+print(result_ternario)
+
+age = 15
+is_adult = 20 if age >= 18 else "string"
+# print(is_adult)
+# is_adult = None
+# if age>= 18:
+#     print(True)
+# else:
+#     print(False)
+
+
+a = [1,2,3,4]
+def doble(num):
+    return num ** 2
+listado = [doble(num) for num in a]
+
+
+
+
+#con comprehension
+result_en_lista = sum([num ** 2 for num in a])
+
+#con generator sintaxis comprehension
+result_en_generador = (num ** 2 for num in a)
+# print(result_en_lista)
+# print(type(result_1))
+
+
+# con función normal
+def dobles(given_list):
+    result = []
+    for num in given_list:
+        result.append(num ** 2)
+    return result
+
+#con función generadora
+def dobles_generator(given_list):
+    for num in given_list:
+        yield num ** 2
+
+result_2 = dobles(a)
+
+
+# con la función de Ricardo:
+result_map = map(lambda num: num ** 2, a)
+
+
+a = [1,2,3,4]
+
+result = [num ** 2 for num in a]
+result = [num** 2 for  num in a  if num%2==0]
+result = [num** 2 if num%2==0 else num for num in a]
+
+num = 10
+
+result = "Es 10" if num == 10 else "Es 100" if num == 100 else "Es 1000" if num == 1000 else "Es 100000" if num == 10000 else "No sé qué número es"
+print(result)
