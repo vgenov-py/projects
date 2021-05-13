7731d20b6a51e598385c8f9e450247d2e287e9ef
unknown <genovese.work@gmail.com>
2021-05-13 22:02:29 +0200

    13052021

--- deas_508/functions.py
+++ deas_508/functions.py
@@ -1,10 +1,70 @@
+from models import Dea
+import json
+import bcrypt
+
 def menu():
-    print("------------")
+    print("----------------")
     print("1. Crear usuario")
     print("2. Acceder")
     print("3. Admins")
     print("4. Salir")
-    print("------------")
+    print("----------------")
+
+#LECTURA Y ESCRITURA DE FICHEROS JSON:
+
+def read_data(fichero):
+    with open(fichero, encoding="utf8") as file:
+        return json.load(file)
+
+def write_data(to_write, fichero):
+    with open(fichero, "w", encoding="utf8") as file:
+        json.dump(to_write, file, indent=4, ensure_ascii=False)
+    print("Guardado correctamente")
+
+#1. CREAR USUARIO:
+def create_user(name, password):
+    new_user = {"name": name, "password": password}
+    return new_user
+
+#2. LOGUEAR USUARIO:
+def log_in(name, password, fichero):
+    users = read_data(fichero)
+    is_user = next(filter(lambda user: user["name"] == name,users["data"]), False)
+    is_password = is_user["password"] if is_user else False
+    veredict = False
+    if is_password:
+        veredict = True if bcrypt.checkpw(password.encode(), is_password.encode()) else veredict
+    return veredict
 
 def choose(msg = "Elija una opción: "):
     return input(msg)
+
+def sub_menu_access():
+    print("--------------------")
+    print("1. Buscar por código")
+    print("2. DEA más cercano")
+    print("--------------------")
+
+def dea_by_id(dea_code, dataset):
+    result = filter(lambda dea: dea["codigo_dea"] == dea_code, dataset)
+    return next(result, "El DEA no está")
+
+def nearest_dea(user_x, user_y, dataset):
+    user_x = int(user_x)
+    user_y = int(user_y)
+    result = None
+    first_dea = dataset[0]
+    coord_x = "direccion_coordenada_x"
+    coord_y = "direccion_coordenada_y"
+    first_dea_object = Dea(int(first_dea[coord_x]), int(first_dea[coord_y]))
+    distance_to_beat = first_dea_object.distance(user_x, user_y)
+
+    for dea in dataset:
+        dea_object = Dea(int(dea[coord_x]), int(dea[coord_y]))
+        dea_distance = dea_object.distance(user_x, user_y)
+        if dea_distance <= distance_to_beat:
+            result = dea
+            distance_to_beat = dea_distance
+        else:
+            continue
+    return result, distance_to_beat
--- deas_508/main.py
+++ deas_508/main.py
@@ -4,42 +4,60 @@ import os
 import utm
 import geocoder
 import functions as func
-
+import requests
+import bcrypt
 # response =req.get("https://datos.comunidad.madrid/catalogo/dataset/35609dd5-9430-4d2e-8198-3eeb277e5282/resource/c38446ec-ace1-4d22-942f-5cc4979d19ed/download/desfibriladores_externos_fuera_ambito_sanitario.json").json()["data"]
 # with open("deas.json", "w", encoding="utf8") as file:
 #     json.dump(response,file, ensure_ascii=False, indent=4)
-di_path = os.path.realpath(__file__)[0:-8]
 
-g = geocoder.ip('me')
-print(g.json)
+di_path = os.path.realpath(__file__)[0:-7]
+users_json = f"{di_path}users.json"
+# print(g.json)
 #  y = "4475002", x = "443123" 
-print(utm.to_latlon(443123, 4475002, 30, "N"))
-print(utm.from_latlon(0, 0))
-
+# print(utm.to_latlon(443123, 4475002, 30, "N"))
+# print(utm.from_latlon(0, 0))
 
 def get_data():
     with open(f"{di_path}\deas.json", encoding="utf8") as file:
         return json.load(file)
 
 data = get_data()
-
 func.menu()
 user = func.choose()
 while user != "4":
-    if user == "1":
-        print("acceder")
-        user = func.choose()
-    elif user == "2":
-        func.sub_menu_access()
-        user = func.choose()
-        if user == "1": # Buscar por código
-            dea_code = func.choose("Indique el código del DEA: ")
-            result = func.dea_by_id(dea_code, data)
-            print(result)
+    if user == "1": #CREATE USER
+        name = func.choose("Nombre: ")
+        password = func.choose("Contraseña: ").encode()
+        password = bcrypt.hashpw(password,bcrypt.gensalt())
+        new_user = func.create_user(name, password.decode())
+        users = func.read_data(users_json)
+        users["data"].append(new_user)
+        func.write_data(users, users_json)
+        user = "token"
+    elif user == "2" or user == "token": # LOGUEAR USUARIO
+        if user == "2":
+            name = func.choose("Usuario: ")
+            password = func.choose("Password: ")
+            veredict = func.log_in(name, password, users_json)
+        elif user == "token":
+            veredict = True
+        if veredict:
+            func.sub_menu_access()
             user = func.choose()
-        elif user == "2": # DEA más cercano y = "4475002", x = "443123"   Y: 4473337 X: 440561
-            user_x = func.choose("Indique coordenada X: ")
-            user_y = func.choose("Indique coordenada y: ")
-            result, distance = func.nearest_dea(user_x, user_y,data)
-            print(f"DEA -->{result}\nDISTANCIA --> {distance}\n Maps-->")
+            if user == "1": # Buscar por código
+                dea_code = func.choose("Indique el código del DEA: ")
+                result = func.dea_by_id(dea_code, data)
+                print(result)
+                func.menu()
+                user = func.choose()
+            elif user == "2": # DEA más cercano y = "4475002", x = "443123"   Y: 4473337 X: 440561
+                user_x = func.choose("Indique coordenada X: ")
+                user_y = func.choose("Indique coordenada Y: ")
+                result, distance = func.nearest_dea(user_x, user_y,data)
+                print(f"DEA -->{result}\nDISTANCIA --> {distance}\n Maps-->")
+                func.menu()
+                user = func.choose()
+        else:
+            print("Usuario o contraseña incorrectos")
+            func.menu()
             user = func.choose()
